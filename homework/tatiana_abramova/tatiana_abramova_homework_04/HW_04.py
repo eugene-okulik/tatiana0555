f9cfb753ce2c1bb7933f11911b2747c43d69813f
tabramova <abramova0555@mail.ru>
2025-07-22 10:58:31 +0300

    tatiana abramova homework_04 done

---
+++ homework/tatiana_abramova/tatiana_abramova_homework_04/HW_04.py
@@ -0,0 +1,24 @@
+# Создайте словарь с такими ключами: ‘tuple’, ‘list’, ‘dict’, ‘set’ (и сохраните его в переменную my_dict)
+
+
+my_dict = {
+    'tuple': (11, 12, 13, 14, 15, 16),
+    'list': ['a', 'b', 30, 40, True],
+    'dict': {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5},
+    'set': {'A', 100, 200, 300, 400, 500}
+}
+
+last_tuple_element = my_dict['tuple'][-1]
+print(f'Последний элемент кортежа внутри словаря: {last_tuple_element}')
+
+my_dict['list'].append(50)
+my_dict['list'].pop(1)
+
+my_dict['dict'][('i am a tuple',)] = 6
+my_dict['dict'].pop('a')
+
+my_dict['set'].add('B')
+my_dict['set'].pop()
+
+print(my_dict)
+print("Итоговый словарь:", my_dict)
