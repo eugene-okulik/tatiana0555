8fbaa586b4b159eb72b3481b32492520aa8ed049
tabramova <abramova0555@mail.ru>
2025-07-25 02:24:24 +0300

    Tatiana Abramova Homework_05 done

---
+++ homework/tatiana_abramova/tatiana_abramova_homework_05/HW_05_file_03.py
@@ -0,0 +1,7 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+students = ', '.join(students)
+subjects = ', '.join(subjects)
+
+print(f'Students {students} study these subjects: {subjects}.')
